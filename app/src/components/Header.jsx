e3cb1dcef2e07c1fef06d985abe6e80050763870
fcordovav <felipe.cordova@alumnos.uach.cl>
2025-04-24 20:14:19 -0400

    icono user

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { User } from 'lucide-react';
+
 
 const Header = () => {
 	return (
 		<header>
 			<nav className="navbar">
 				<div className="flex flex-row justify-around items-center lg:h-15">
-					<a href="/" className="logo	">Votación Blockchain</a>
+					<a href="/" className="logo	"><img src="/logo.png" alt="Logo de la página" className="h-10 w-auto" /></a>
 					<ul className="nav-links flex lg:flex-row lg:space-x-20">
-						<li><a href="#acerca-de">acerca de</a></li>
-						<li><a href="#como-votar">como votar</a></li>
-						<li><a href="#preguntas-frecuentes">preguntas frecuentes</a></li>
-						<li><a href="/login">Login</a></li>
+						<li><a href="#acerca-de">Acerca de</a></li>
+						<li><a href="#como-votar">Cómo votar</a></li>
+						<li><a href="#noticias">Noticias</a></li>
+						<li><a href="#preguntas-frecuentes">Preguntas frecuentes</a></li>
+						<li><a href="/login" className="flex items-center justify-center"><User className="w-6 h-6 align-middle" /></a></li>
 					</ul>
 				</div>
 			</nav>
--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -11,21 +11,25 @@ const Home = () => {
 						<h1 className='lg:text-5xl'>Sistema de Votación Blockchain</h1>
 						<p>Bienvenido al sistema de votación basado en blockchain. Aquí puedes participar en elecciones de manera segura y transparente.</p>
 						<div className='lg:py-10'>
-							<a href="/votar" className="btn border-1 rounded-xl px-6 py-3 ">Votar</a>
+							<a href="/login" className="btn border-1 rounded-xl px-6 py-3 ">Votar</a>
 						</div>
 					</div>
 				</section>
 				
 				<section id='acerca-de'>
-					<h1>acerca de</h1>
+					<h1>Acerca de</h1>
 				</section>
 
 				<section id='como-votar'>
-					<h1>como votar</h1>
+					<h1>Cómo votar</h1>
 				</section>
 
 				<section id='preguntas-frecuentes'>
-					<h1>preguntas frecuentes</h1>
+					<h1>Preguntas frecuentes</h1>
+				</section>
+
+				<section id='noticias'>
+					<h1>Noticias</h1>
 				</section>
 			</div>
 		</>
