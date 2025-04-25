6ffcb24b3852636a2f3ea141199ca5a0889df922
CreativeSelf1 <fernacastillo.perez0607@gmail.com>
2025-04-24 21:26:44 -0400

    cambios margenes

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -7,11 +7,11 @@ const Home = () => {
 	return (
 		<>
 			<Header />
-			<div className="home lg:p-10">
-				<section className='hero lg:h-[90vh] flex items-center'>
-					<div className=''>
-						<h1 className='lg:text-5xl'>Sistema de Votación Blockchain</h1>
-						<p>Bienvenido al sistema de votación basado en blockchain. Aquí puedes participar en elecciones de manera segura y transparente.</p>
+			<div className="home p-10 xl:p-20">
+				<section className='hero h-screen lg:h-[90vh] flex items-center'>
+					<div>
+						<h1 className='text-6xl xl:text-7xl'>UNIBLOCK</h1>
+						<p className='py-10 xl:max-w-150'>Bienvenido al sistema de votación basado en blockchain. Aquí puedes participar en elecciones de manera segura y transparente.</p>
 						<div className='lg:py-10'>
 							<a href="/login" className="btn border-1 rounded-xl px-6 py-3 ">Votar</a>
 						</div>
@@ -22,8 +22,13 @@ const Home = () => {
 					<h1>Acerca de</h1>
 				</section>
 
-				<section id='como-votar'>
+				<section id='como-votar' className='my-10 lg:py-30'>
 					<h1>Cómo votar</h1>
+						<div className='flex flex-col justify-center items-center lg:flex-row gap-10 xl:gap-40'>
+						<div className='paso1 w-24 h-24  border-1 rounded-full p-10 flex items-center justify-center'>1</div>
+						<div className='paso2 w-24 h-24  border-1 rounded-full p-10 flex items-center justify-center'>2</div>
+						<div className='paso3 w-24 h-24  border-1 rounded-full p-10 flex items-center justify-center'>3</div>
+					</div>
 				</section>
 
 				<section id='noticias'>
