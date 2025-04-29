bb60f5caa7df1c3f72d83399b11510d4c63dfcd0
CreativeSelf1 <fernacastillo.perez0607@gmail.com>
2025-04-29 11:00:41 -0400

    conexion a base de datos RDS amazon

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import { authRouter } from './src/routes/Auth.js'; 
+import pool from './src/config/postgresConnect.js'; // Asegúrate de que la ruta sea correcta
 
 dotenv.config();
 const app = express();
@@ -21,6 +22,16 @@ app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Bienvenido a la página principal');
+});
+
+app.get('/api/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ message: 'Conexión exitosa', time: result.rows[0] });
+  } catch (error) {
+    console.error('Error al consultar la base de datos:', error);
+    res.status(500).json({ message: 'Error en la conexión a la base de datos', error });
+  }
 });
 
 
