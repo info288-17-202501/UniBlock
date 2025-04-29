26b1a355e8b2052504d08733a0c23736fdc41fc6
fcordovav <felipe.cordova@alumnos.uach.cl>
2025-04-29 19:15:15 -0400

    login con microsoft, falta ajustar los puertos

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,53 @@
-import express from 'express';
-import cors from 'cors'; 
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
-import { authRouter } from './src/routes/Auth.js'; 
+// import express from 'express';
+// import cors from 'cors'; 
+// import cookieParser from 'cookie-parser';
+// import dotenv from 'dotenv';
+// import { authRouter } from './src/routes/Auth.js'; 
 
-dotenv.config();
+// dotenv.config();
+// const app = express();
+// const port = 3001;
+
+// const corsOptions = {
+//   origin: 'http://localhost:5173', // URL de tu frontend
+//   credentials: true, // Permitir el uso de credenciales (como cookies)
+// };
+
+// app.use(cors(corsOptions)); // Usar CORS con las opciones configuradas
+
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.get('/', (req, res) => {
+//   res.send('Bienvenido a la página principal');
+// });
+
+
+// // ENDPOINTS
+// app.use("/api/auth", authRouter);
+
+
+// app.use((req, res) => {
+//   res.status(404).json({ message: "Ruta no encontrada" });
+// });
+
+
+// app.listen(port, () => {
+//   console.log(`Servidor con CORS corriendo en http://localhost:${port}`);
+// });
+
+
+
+import express from "express";
+import { loginRouter } from "./src/routes/microsoft.js";
+import passport from "passport";
+import "./src/middlewares/microsoft.js";
+
 const app = express();
-const port = 3001;
 
-const corsOptions = {
-  origin: 'http://localhost:5173', // URL de tu frontend
-  credentials: true, // Permitir el uso de credenciales (como cookies)
-};
+app.use(passport.initialize());
 
-app.use(cors(corsOptions)); // Usar CORS con las opciones configuradas
+app.use("/auth", loginRouter);
 
-
-app.use(express.json());
-app.use(cookieParser());
-
-app.get('/', (req, res) => {
-  res.send('Bienvenido a la página principal');
-});
-
-
-// ENDPOINTS
-app.use("/api/auth", authRouter);
-
-
-app.use((req, res) => {
-  res.status(404).json({ message: "Ruta no encontrada" });
-});
-
-
-app.listen(port, () => {
-  console.log(`Servidor con CORS corriendo en http://localhost:${port}`);
-});
+app.listen(3000, () => console.log("http://localhost:3000"));
--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -133,6 +133,48 @@ const Auth = () => {
                 {isLogin ? 'Regístrate aquí' : 'Inicia sesión aquí'}
               </button>
             </div>
+
+
+            {/* TODO ESTE DIV ES DEL BOTON DE MICROSOFT */}
+            <div className = "mt-6">
+              <button
+                    onClick={() => {
+                      const popup = window.open(
+                        "http://localhost:3000/auth/microsoft",
+                        "targetWindow",
+                        `toolbar=no,
+                        location=no,
+                        status=no,
+                        menubar=no,
+                        scrollbars=yes,
+                        resizable=yes,
+                        width=620,
+                        height=700`
+                      );
+
+                      window.addEventListener("message", (event) => {
+                        if (event.origin === "http://localhost:3000") {
+                          if (event.data) {
+                            sessionStorage.setItem("user", JSON.stringify(event.data));
+                            popup.close();
+                          }
+                        }
+                      });
+                    }}
+                    className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                    <img
+                      src="https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg"
+                      alt="Microsoft logo"
+                      className="w-5 h-5 mr-2"
+                    />
+                    <span className="text-sm text-center leading-tight">
+                      Continuar con cuenta<br />Microsoft UACh
+                    </span>
+              </button>
+            </div>
+
+
+
           </div>
         </div>
       </div>
